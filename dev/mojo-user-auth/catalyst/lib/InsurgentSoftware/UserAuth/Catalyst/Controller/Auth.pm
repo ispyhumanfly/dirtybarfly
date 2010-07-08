fd93516771164a9a0fda593f10b51a5916ef1c41
Shlomi Fish <shlomif@iglu.org.il>
2010-07-08 21:52:14 +0300

    Add Controller::Auth.
    This is where we will put the authentication controller.

---
+++ dev/mojo-user-auth/catalyst/lib/InsurgentSoftware/UserAuth/Catalyst/Controller/Auth.pm
@@ -0,0 +1,80 @@
+package InsurgentSoftware::UserAuth::Catalyst::Controller::Auth;
+
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+# Temporary - for :
+    # use InsurgentSoftware::UserAuth::User;
+    # use InsurgentSoftware::UserAuth::App;
+
+use lib '../';
+
+use InsurgentSoftware::UserAuth::User;
+use InsurgentSoftware::UserAuth::App;
+
+#
+# Sets the actions in this controller to be registered with no prefix
+# so they function identically to actions created in MyApp.pm
+#
+__PACKAGE__->config(namespace => '');
+
+=head1 NAME
+
+InsurgentSoftware::UserAuth::Catalyst::Controller::Auth - 
+Root Controller for the authentication.
+
+=head1 DESCRIPTION
+
+[enter your description here]
+
+=head1 METHODS
+
+=head2 index
+
+The root page (/)
+
+=cut
+
+sub index :Path('/auth') :Args(0) {
+    my ( $self, $c ) = @_;
+
+    # Hello World
+    $c->response->body( "Hello There Lately");
+}
+
+=head2 default
+
+Standard 404 error page
+
+=cut
+
+sub default :Path {
+    my ( $self, $c ) = @_;
+    $c->response->body( 'Page not found' );
+    $c->response->status(404);
+}
+
+=head2 end
+
+Attempt to render a view, if needed.
+
+=cut
+
+sub end : ActionClass('RenderView') {}
+
+=head1 AUTHOR
+
+Catalyst developer
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
